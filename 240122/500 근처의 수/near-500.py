a2b1dc6b321b7c9b661c07eb3116424ef3805b71
Lee haejun <teamforhj@gmail.com>
2024-01-22 22:58:23 +0900

    Title: 500 근처의 수, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/500 근처의 수/near-500.py
@@ -0,0 +1,14 @@
+def discriminant_neg(x):
+    return x<0
+def discriminant_pos(x):
+    return x>0
+
+
+array = list(map(int, input().split()))
+
+array = [e-500 for e in array]
+
+neg_array = list(filter(discriminant_neg, array))
+pos_array = list(filter(discriminant_pos, array))
+
+print(f"{neg_array[-1]+500} {pos_array[0]+500}")
