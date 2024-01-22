7d41da9c73430e1c5abe2818b7d411e2917026ef
Lee haejun <teamforhj@gmail.com>
2024-01-22 22:28:55 +0900

    Title: 999 또는 -999, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/999 또는 -999/999-or-999.py
+++ 240122/999 또는 -999/999-or-999.py
@@ -1,6 +1,6 @@
 array = list(map(int, input().split()))
 
-idx_999 = array.index(999) if array.index(999) else array.index(-999)
+idx_999 = array.index(999) if 999 in array else array.index(-999)
 a = array[0:idx_999]
 
 print(f"{max(a)} {min(a)}")
