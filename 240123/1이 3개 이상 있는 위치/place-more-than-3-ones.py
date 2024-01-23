18412dd002325593579e90fb5ab92457568def01
Lee haejun <teamforhj@gmail.com>
2024-01-23 12:02:02 +0900

    Title: 1이 3개 이상 있는 위치, Time: 173ms, Memory: 25MB, Status: Passed - CodeTree

--- 240123/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240123/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -17,14 +17,14 @@ dy = [1,0,-1,0]
 result = 0
 
 for i in range(n):
-    count = 0
     for j in range(n):
+        count = 0
         for a,b in zip(dx, dy):
             x = i + a
             y = j + b
             if in_range(x,y) and square[x][y] == 1:
                 count += 1
-    if count >= 3:
-        result += 1
+        if count >= 3:
+            result += 1
 
 print(result)
