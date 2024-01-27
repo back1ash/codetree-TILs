bb5a84889a22049b2ed5734924ef6aee32bf33a2
Lee haejun <teamforhj@gmail.com>
2024-01-27 21:08:47 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240127/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,15 @@
+n,t = map(int, input().split())
+
+belts = [list(map(int, input().split())) for _ in range(2)]
+
+now = 0
+while now < t:
+    tmp = [belts[0][2], belts[1][2]]
+    for a in range(2):
+        for i in range(n-1,0,-1):
+            belts[a][i] = belts[a][i-1]
+    belts[1][0], belts[0][0] = tmp 
+    now += 1
+
+for belt in belts:
+    print(" ".join(map(str, belt)))
