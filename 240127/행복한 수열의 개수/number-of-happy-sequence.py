9542638b1b3a9c4411e8cfefe49d9875b17a5a00
Lee haejun <teamforhj@gmail.com>
2024-01-27 20:48:52 +0900

    Title: 행복한 수열의 개수, Time: 177ms, Memory: 26MB, Status: Passed - CodeTree

--- 240127/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240127/행복한 수열의 개수/number-of-happy-sequence.py
@@ -10,17 +10,30 @@ for y in range(n):
     for x in range(n):
         crnt = square[y][x]
         x_count = 0
-        y_count = 0
         for dx in range(m):
-            if in_range(x+dx, y) and square[y][x+dx] != crnt:
-                x_count += 1
+            if not in_range(x+dx, y):
+                break
+            if square[y][x+dx] != crnt:
+                break
+            x_count += 1
         if x_count >= m:
             happy += 1
+            break
 
+
+
+for x in range(n):
+    for y in range(n):
+        crnt = square[y][x]
+        y_count = 0
         for dy in range(m):
-            if in_range(x, y+dy) and square[y+dy][x] == crnt:
-                y_count += 1
+            if not in_range(x, y+dy):
+                break
+            if square[y+dy][x] != crnt:
+                break
+            y_count += 1
         if y_count >= m:
             happy += 1
-
+            break
+        
 print(happy)
