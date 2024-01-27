152d1583a9c38bac8ed30d8d08468fc58ef09b1a
Lee haejun <teamforhj@gmail.com>
2024-01-27 21:09:34 +0900

    Title: 컨베이어 벨트, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240127/컨베이어 벨트/conveyor-belt.py
+++ 240127/컨베이어 벨트/conveyor-belt.py
@@ -4,7 +4,7 @@ belts = [list(map(int, input().split())) for _ in range(2)]
 
 now = 0
 while now < t:
-    tmp = [belts[0][2], belts[1][2]]
+    tmp = [belts[0][-1], belts[1][-1]]
     for a in range(2):
         for i in range(n-1,0,-1):
             belts[a][i] = belts[a][i-1]
