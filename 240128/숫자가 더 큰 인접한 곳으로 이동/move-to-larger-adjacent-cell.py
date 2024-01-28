34da4f012aaad22adc7759fc320eb939f8f1471a
Lee haejun <teamforhj@gmail.com>
2024-01-28 16:54:03 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240128/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+r, c = (r-1, c-1)
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+square = [list(map(int, input().split())) for _ in range(n)]
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+visit_list = []
+while True:
+    direction = 4
+    now = square[r][c]
+    visit_list.append(now)
+    for i in range(4):
+        if in_range(r+dx[i], c+dy[i]) and now < square[r+dy[i]][c+dx[i]]:
+            now = square[r+dy[i]][c+dx[i]]
+            direction = i
+            break
+    if direction == 4:
+        break
+
+    r, c = (r+dy[direction], c+dx[direction])
+
+print(' '.join(map(str, visit_list)))
