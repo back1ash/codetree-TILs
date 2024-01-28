f377cf44e976c0c225716344af61972bd55b8afb
Lee haejun <teamforhj@gmail.com>
2024-01-28 16:20:20 +0900

    Title: 십자 모양 폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240128/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+square = [list(map(int, input().split())) for _ in range(n)]
+
+r, c = map(int, input().split())
+r, c = (r-1, c-1)
+scope = square[r][c]
+dx = [1, 0, 0, -1]
+dy = [0, 1, -1, 0]
+
+#boom
+for a in range(scope):
+    for i in range(4):
+        square[r+(dx[i]*a)][c+(dy[i]*a)] = 0
+
+tmp = [[0 for _ in range(n)]for _ in range(n)]
+#gravity
+for x in range(n):
+    cur = 3
+    for y in range(n-1, -1, -1):
+        if square[y][x] != 0:
+            tmp[cur][x] = square[y][x]
+            cur -= 1
+
+square = tmp
+for row in square:
+    print(' '.join(map(str, row)))
