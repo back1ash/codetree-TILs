2967590c6030c31138ee51f51d359e59ae98a84d
Lee haejun <teamforhj@gmail.com>
2024-01-28 17:25:26 +0900

    Title: 떨어지는 1자 블록, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240128/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240128/떨어지는 1자 블록/falling-horizontal-block.py
@@ -5,8 +5,8 @@ n, m, k = map(int, input().split())
 square = [list(map(int, input().split())) for _ in range(n)]
 k = k-1
 
+flag = 0
 for row in range(n):
-    flag = 0
     for a in range(m):
         if square[row][k+a] != 0:
             square[row-1][k:k+m] = [1 for _ in range(m)]
@@ -14,6 +14,9 @@ for row in range(n):
             break
     if flag == 1:
         break
+
+if flag == 0:
+    square[-1][k:k+m] = [1 for _ in range(m)]
 
 for row in square:
     print(' '.join(map(str, row)))
