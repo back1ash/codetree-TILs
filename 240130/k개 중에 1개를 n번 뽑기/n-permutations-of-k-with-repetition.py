2809fa3e163736c107513793f8dc58edc9a8677f
Lee haejun <teamforhj@gmail.com>
2024-01-30 20:41:03 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 226ms, Memory: 28MB, Status: Passed - CodeTree

--- 240130/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 240130/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -8,7 +8,7 @@ def choose(curr_num):
     if curr_num == n+1:
         print_answer(answer)
         return
-    for i in range(1,n+1):
+    for i in range(1,k+1):
         answer.append(i)
         choose(curr_num + 1)
         answer.pop()
