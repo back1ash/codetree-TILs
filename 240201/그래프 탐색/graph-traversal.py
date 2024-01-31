f3d68f994105967246f9bc33bd590092fb57116f
Lee haejun <teamforhj@gmail.com>
2024-02-01 00:41:00 +0900

    Title: 그래프 탐색, Time: 245ms, Memory: 33MB, Status: Passed - CodeTree

--- 240201/그래프 탐색/graph-traversal.py
+++ 240201/그래프 탐색/graph-traversal.py
@@ -2,6 +2,7 @@ n, m = map(int, input().split())
 
 visited = [0 for _ in range(n+1)]
 graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+visited[1] = 1
 
 for _ in range(m):
     x,y = map(int, input().split())
@@ -9,7 +10,7 @@ for _ in range(m):
     graph[y][x] = 1
 
 def dfs(now):
-    for vertex in range(n):
+    for vertex in range(1,n+1):
         if graph[now][vertex] and not visited[vertex]:
             visited[vertex] = 1
             dfs(vertex)
