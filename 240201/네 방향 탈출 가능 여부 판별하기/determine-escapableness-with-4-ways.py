457490744c2e04251507aa432aa2cdab7e6fa517
Lee haejun <teamforhj@gmail.com>
2024-02-01 01:41:38 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 216ms, Memory: 26MB, Status: Passed - CodeTree

--- 240201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -11,7 +11,7 @@ maze = [list(map(int, input().split())) for _ in range(n)]
 maze[0][0] = 0
 
 def in_range(x, y):
-    return 0<=x<m and 0<=y<n
+    return 0<=x<n and 0<=y<m
 
 def bfs():
     while q:
