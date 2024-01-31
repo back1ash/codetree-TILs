b36b2b242b4979010f79896e0367652c09828363
Lee haejun <teamforhj@gmail.com>
2024-02-01 01:33:33 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -2,29 +2,25 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def solve():
-    q = deque()
-    dxs = [-1, 1, 0, 0]
-    dys = [0, 0, -1 ,1]
+q = deque()
+dxs = [-1, 1, 0, 0]
+dys = [0, 0, -1 ,1]
 
-    n,m = map(int, input().split())
-    maze = [list(map(int, input().split())) for _ in range(m)]
-    if maze[m-1][n-1] == 1:
-        print(0)
-        return
+n,m = map(int, input().split())
+maze = [list(map(int, input().split())) for _ in range(m)]
+maze[0][0] = 0
 
-    def in_range(x, y):
-        return 0<=x<m and 0<=y<n
+def in_range(x, y):
+    return 0<=x<m and 0<=y<n
 
-    def bfs():
-        while q:
-            x, y = q.popleft()
-            for dx, dy in zip(dxs, dys):
-                if in_range(x+dx, y+dy) and not maze[x+dx][y+dy]:
-                    q.append([x+dx, y+dy])
-                    maze[x+dx][y+dy] = 1
-    q.append([0,0])
-    bfs()
-    print(1 if maze[m-1][n-1] == 1 else 0)
-
-solve()
+def bfs():
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            cx, cy = (x+dx, y+dy)
+            if in_range(cx, cy) and maze[cx][cy]:
+                q.append([cx, cy])
+                maze[cx][cy] = 0
+q.append([0,0])
+bfs()
+print(1 if maze[m-1][n-1] == 0 else 0)
